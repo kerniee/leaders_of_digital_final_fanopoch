7e2d4b6a59fe0e0039aa3132912d6841cd1b259c
Alexander Batyrgariev <sasha@bashkort.org>
2020-11-28 05:44:18 +0300

    Added ordering and filters

--- leader_final_site/main/urls.py
+++ leader_final_site/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import CardsListView
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("cards", CardsListView.as_view(), name="cards")
 ]
--- leader_final_site/main/views.py
+++ leader_final_site/main/views.py
@@ -1,6 +1,40 @@
+import django_filters
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from django_filters.views import FilterView
+
+from main.models import Card
 
 
 def index(request):
     return render(request, "main/index.html")
+
+
+class CardsFilter(django_filters.FilterSet):
+    o = django_filters.OrderingFilter(
+        fields=(
+            ('priority', 'priority'),
+            ('deadline', 'deadline'),
+            ('finished_at', 'finished_at'),
+            ('created_at', 'created_at')
+        ),
+        field_labels={
+            'priority': 'Приоритет',
+            'deadline': 'Дедлайн',
+            'finished_at': 'Завершено',
+            'created_at': 'Создано'
+        }
+    )
+
+    class Meta:
+        model = Card
+        fields = ['priority', 'creator', 'cls', 'type']
+
+
+class CardsListView(FilterView):
+    paginate_by = 10
+    template_name = 'main/cards.html'
+    model = Card
+    context_object_name = 'cards'
+    filterset_class = CardsFilter
