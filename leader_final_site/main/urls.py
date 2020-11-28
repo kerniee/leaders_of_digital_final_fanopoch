68c33a26385ceb92d1895d09e1037d6a764c9758
kernie <karapys.mor@gmail.com>
2020-11-28 03:39:54 +0300

    first draft of design

--- leader_final_site/main/urls.py
+++ leader_final_site/main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("requests/", views.requests, name="requests"),
+    path("messages/", views.messages, name="messages"),
+    path("missions/", views.missions, name="missions"),
 ]
--- leader_final_site/main/views.py
+++ leader_final_site/main/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def get_down_menu_data():
+    data = [
+        ("Заявки", "requests", 22),
+        ("Сообщения", "messages", 0),
+        ("Поручения", "missions", 2)
+    ]
+    return data
+
+
 def index(request):
-    return render(request, "main/index.html")
+    return render(request, "main/index.html", {"data": get_down_menu_data()})
+
+
+def requests(request):
+    return render(request, "main/requests.html", {"data": get_down_menu_data()})
+
+
+def messages(request):
+    return render(request, "main/messages.html", {"data": get_down_menu_data()})
+
+
+def missions(request):
+    return render(request, "main/missions.html", {"data": get_down_menu_data()})
