8c74793441d304889250d1e8382268d53c0b59a7
Alexander Batyrgariev <sasha@bashkort.org>
2020-11-28 23:54:56 +0300

    Removed unnecessary filter

--- leader_final_site/main/views.py
+++ leader_final_site/main/views.py
@@ -123,7 +123,6 @@ class CardsCreateView(CreateView, LoginRequiredMixin):
     model = Card
     fields = (
         'header',
-        'cls',
         'type',
         'priority',
         'deadline',
